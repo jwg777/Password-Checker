d304acc5a9974d0f15e4cf0ade186fdb4cd98d16
joshuagargan <zfac209@live.rhul.ac.uk>
2020-10-28 14:12:49 +0000

    Added basic test suite to consider most cases for password checker class

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
     PasswordChecker check = new PasswordChecker();
     
-    System.out.println(check.passwordCriteriaCheck("Test123!longer"));
+    System.out.println(check.passwordCriteriaCheck("Password123"));
   }
   
 }
--- src/testPackage/TestPasswordChecker.java
+++ src/testPackage/TestPasswordChecker.java
@@ -1,8 +1,72 @@
 package testPackage;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import mainPackage.PasswordChecker;
 
 class TestPasswordChecker {
+  
+  private PasswordChecker checkPassword;
+  
+  @BeforeEach
+  public void setUp() {
+    checkPassword = new PasswordChecker();
+  }
+  
+  @Test
+  public void testStrongPassword() {
+    String result = checkPassword.passwordCriteriaCheck("Password123!");
+    assertEquals(result, "Password is strong!");
 
+  }
+  
+  @Test
+  public void testEmptyPassword() {
+    String result = checkPassword.passwordCriteriaCheck("");
+    assertEquals(result, "The length must be at least 8.\n" + 
+        "It must contain at least one digit.\n" + 
+        "It must contain at least one lowercase English character.\n" + 
+        "It must contain at least one uppercase English character.\n" + 
+        "It must contain at least one special character. The special characters are: !@#$%^&*()-+");
+  }
+  
+  @Test
+  public void testNoDigit() {
+    String result = checkPassword.passwordCriteriaCheck("Passworddddd!");
+    assertEquals(result, "\n" + 
+        "It must contain at least one digit.");
+
+  }
+  
+  @Test
+  public void testNoLowerCase() {
+    String result = checkPassword.passwordCriteriaCheck("PASSWORD123!");
+    assertEquals(result, "\n" + 
+        "It must contain at least one lowercase English character.");
+
+  }
+  
+  @Test
+  public void testNoUpperCase() {
+    String result = checkPassword.passwordCriteriaCheck("password123!");
+    assertEquals(result, "\n" + 
+        "It must contain at least one uppercase English character.");
+
+  }
+  
+  @Test
+  public void testTooShort() {
+    String result = checkPassword.passwordCriteriaCheck("Pass1!");
+    assertEquals(result, "The length must be at least 8.");
+
+  }
+  
+  @Test
+  public void testNoSymbol() {
+    String result = checkPassword.passwordCriteriaCheck("Password123");
+    assertEquals(result, "\n" + 
+        "It must contain at least one special character. The special characters are: !@#$%^&*()-+");
+
+  }
 }
